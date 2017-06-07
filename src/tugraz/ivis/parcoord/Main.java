ba15d5917bf1542f238cda12db4b020419e6c11e
Thorsten Ruprechter <th.ruprechter@gmail.com>
2017-06-07 12:04:21 +0200

    changed project title

--- src/tugraz/ivis/parcoord/Main.java
+++ src/tugraz/ivis/parcoord/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("parcoord.fxml"));
         Parent root = loader.load();
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Parallel Coordinates Test");
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
 
